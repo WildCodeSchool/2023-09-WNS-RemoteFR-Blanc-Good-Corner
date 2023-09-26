a910f02eee6e62fe7b1bd3c8f3ab5a58f46ffc4f
Mael <mael.vincent.pro@gmail.com>
2023-09-26 16:27:51 +0200

    useEffect + fetch

--- frontend/src/components/RecentAds.tsx
+++ frontend/src/components/RecentAds.tsx
@@ -1,54 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AdCard from "./AdCard";
 import styles from "@/styles/RecentAds.module.css";
+import { Ad } from "@/types/ad.type";
 
 export default function RecentAds() {
   const [totalPrice, setTotalPrice] = useState<number>(0);
+  const [ads, setAds] = useState<Ad[]>([]); // AdCardProps[] is the type of the ads
 
-  const ads = [
-    {
-      title: "Table",
-      picture: "/images/table.webp",
-      price: 120,
-      link: "/ads/table"
-    },
-    {
-      title: "Dame Jeanne",
-      picture: "/images/dame-jeanne.webp",
-      price: 75,
-      link: "/ads/dame-jeanne"
-    },
-    {
-      title: "Vide poche",
-      picture: "/images/vide-poche.webp",
-      price: 4,
-      link: "/ads/vide-poche"
-    },
-    {
-      title: "Table",
-      picture: "/images/table.webp",
-      price: 120,
-      link: "/ads/table"
-    },
-    {
-      title: "Vaisselier",
-      picture: "/images/vaisselier.webp",
-      price: 900,
-      link: "/ads/vaisselier"
-    },
-    {
-      title: "Bougie",
-      picture: "/images/bougie.webp",
-      price: 2,
-      link: "/ads/bougie"
-    },
-    {
-      title: "Porte magazine",
-      picture: "/images/porte-magazine.webp",
-      price: 45,
-      link: "/ads/porte-magazine"
-    },
-  ];
+  /*fetch("http://localhost:3001/ads")
+    .then((response) => response.json())
+    .then((data) => setAds(data));
+*/
+
+  useEffect(() => {
+    const fetchAds = () => {
+      fetch("http://localhost:3001/ads")
+        .then((response) => response.json())
+        .then((data) => setAds(data));
+    }
+
+    fetchAds();
+  }, []);
 
   const addToTotalPrice = (price: number) => {
     setTotalPrice(totalPrice + price)
@@ -65,7 +37,7 @@ export default function RecentAds() {
               title={ad.title}
               picture={ad.picture}
               price={ad.price}
-              link={ad.link}
+              link={`/ads/${ad.id}`}
             />
             <button className="button" onClick={() => addToTotalPrice(ad.price)}>Add price to total</button>
           </div>
---
+++ frontend/src/types/ad.type.ts
@@ -0,0 +1,6 @@
+export type Ad = {
+  id: number;
+  title: string;
+  picture: string;
+  price: number;
+}
