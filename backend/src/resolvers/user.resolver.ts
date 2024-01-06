91e954ff868168eae43461931fabfd2f40e49362
Mael <mael.vincent.pro@gmail.com>
2024-01-06 08:38:02 +0100

    Add GRaphQL query getMe

--- backend/src/resolvers/user.resolver.ts
+++ backend/src/resolvers/user.resolver.ts
@@ -1,10 +1,17 @@
-import { Arg, Mutation, Resolver } from "type-graphql";
+import { Arg, Authorized, Ctx, Mutation, Query, Resolver } from "type-graphql";
 import { User } from "../entities/user";
 import * as UserService from "../services/user.service";
 import * as AuthService from "../services/auth.service";
 
 @Resolver(User)
 export class UserResolver {
+
+  @Authorized()
+  @Query(() => User)
+  getMe(@Ctx() context: {user: User}): User {
+    return context.user;
+  }
+
   @Mutation(() => User)
   async signUp(
     @Arg("email") email: string,
